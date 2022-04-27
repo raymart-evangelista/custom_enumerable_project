a3f32031d326ca6a1ed5e0bc1fa60c977de89e10
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-04-27 12:56:32 -0700

    turn yield into blk.call for better readability

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,78 +1,74 @@
 module Enumerable
-  def my_each_with_index
-    counter = 0
-    my_each do |obj|
-      yield obj, counter
-      counter += 1
+  def my_each_with_index(&blk)
+    idx = 0
+    my_each do |elem|
+      blk.call(elem, idx)
+      idx += 1
     end
     self
   end
 
-  def my_select
+  def my_select(&blk)
     matched_elems = []
-    my_each do |i|
-      matched_elems.push(i) if yield(i)
+    my_each do |idx|
+      matched_elems.push(idx) if blk.call(idx)
     end
     matched_elems
   end
 
-  def my_all?
+  def my_all?(&blk)
     met_conditions = false
-    my_each do |i|
-      if yield(i)
-        met_conditions = true
-      else
-        met_conditions = false
-      end
+    my_each do |idx|
+      met_conditions = if blk.call(idx)
+                         true
+                       else
+                         false
+                       end
     end
     met_conditions
   end
 
-  def my_any?
-    my_each do |i|
-      if yield(i)
+  def my_any?(&blk)
+    my_each do |idx|
+      if blk.call(idx)
         return true
       end
     end
     false
   end
 
-  def my_none?
-    my_each do |i|
-      if yield(i)
+  def my_none?(&blk)
+    my_each do |idx|
+      if blk.call(idx)
         return false
       end
     end
     true
   end
 
-  def my_count
+  def my_count(&blk)
     count = 0
     if block_given?
-      my_each do |i|
-        if yield(i)
-          count += 1
-        end
+      my_each do |idx|
+        count += 1 if blk.call(idx)
       end
-      return count
-    end
-    my_each do
-      count += 1
+    else
+      my_each { count += 1 }
     end
     count
   end
 
-  def my_map
+  def my_map(&blk)
     new_arr = []
-    my_each do |i|
-      new_arr.push(yield(i))
+    my_each do |idx|
+      new_arr.push(blk.call(idx))
     end
     new_arr
   end
 
-  def my_inject(value)
-    my_each do |i|
-      value = yield(i,value)
+  def my_inject(value, &blk)
+    my_each do |idx|
+      value = blk.call(idx, value)
     end
     value
   end
@@ -84,9 +80,12 @@ end
 # to this method
 class Array
   # Define my_each here
-  def my_each
-    size.times do |i|
-      yield self.at(i)
+  def my_each(&blk)
+    i = 0
+    while i < count
+      elem = self[i]
+      blk.call(elem)
+      i += 1
     end
     self
   end
